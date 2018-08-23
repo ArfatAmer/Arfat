120d569be4b27e85e0fe9e20d3ee1f0dc35d46d9
ArfatAmer <42632820+ArfatAmer@users.noreply.github.com>
2018-08-23 13:09:20 +0530

    lines are reduce

--- BrightIT.java
+++ BrightIT.java
@@ -10,18 +10,4 @@ System.out.println("Bright IT Carrier");
 }
 
 
-
-sufahlasgf
-fgsdlfigs
-asflisudgefds
-safhsdofha
-sfhsdfha;sda
-sdfjsdfijs
-casfjsiddfj
-sfksdofsdof
-fskosdfas
-dsjfs
-jfisdf
-
-
 //This code is use to print 10 times.
